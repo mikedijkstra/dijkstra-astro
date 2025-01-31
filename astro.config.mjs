46d8441e8212d7463a0e87dced93fe3a2fef0870
Michael Dijkstra <michael@dijkstra.com.au>
2025-01-31 21:14:31 +1100

    ⚡ feat: Setup GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import alpinejs from '@astrojs/alpinejs';
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()]
-	},
+    plugins: [tailwindcss()],
+  },
 
-  integrations: [alpinejs()]
+  integrations: [alpinejs()],
+
+  site: "https://mikedijkstra.github.io",
+  base: "/dijkstra-astro",
 });
+
